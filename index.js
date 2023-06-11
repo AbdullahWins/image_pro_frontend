615cb41385d269823d3a043af8ab9e92187e2ac6
Abdullah Al MahMud <abdudevs@gmail.com>
2023-06-11 15:17:18 +0600

    test commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(multer({ dest: "uploads/" }).single("file"));
